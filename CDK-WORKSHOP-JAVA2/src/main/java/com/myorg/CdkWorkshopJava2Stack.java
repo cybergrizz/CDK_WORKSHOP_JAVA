fac0d1f669d2508991049fbfe4fe539a5d9bb071
CyberGrizz88 <cybergrizz88@gmail.com>
2023-08-06 21:31:24 -0400

    Adding Lambda function to stack

--- CDK-WORKSHOP-JAVA2/src/main/java/com/myorg/CdkWorkshopJava2Stack.java
+++ CDK-WORKSHOP-JAVA2/src/main/java/com/myorg/CdkWorkshopJava2Stack.java
@@ -4,6 +4,10 @@ import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.lambda.Runtime;
+
 public class CdkWorkshopJava2Stack extends Stack {
     public CdkWorkshopJava2Stack(final Construct parent, final String id) {
         this(parent, id, null);
@@ -11,6 +15,12 @@ public class CdkWorkshopJava2Stack extends Stack {
 
     public CdkWorkshopJava2Stack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
+
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+        .runtime(Runtime.NODEJS_14_X)
+        .code(Code.fromAsset("lambda"))
+        .handler("hello.handler")
+        .build()
 
     }
 }
