17c82120eb57bc5be3bcd228cee0272b61a44a6c
CyberGrizz88 <cybergrizz88@gmail.com>
2023-08-06 23:58:25 -0400

    Updating message for testing purposes

--- CDK-WORKSHOP-JAVA2/lambda/hello.js
+++ CDK-WORKSHOP-JAVA2/lambda/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function(event) {
 	return {
 		statusCode: 200,
 		headers: { "Contyent-Type": ":text/plain" },
-		body: 'Good Night, CDK! Youve hit ${event.path}\n'
+		body: 'Good Evening ladies\n'
 	};
 }
