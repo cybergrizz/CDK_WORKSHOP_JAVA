8b95b50c4cb55a65178b7d3fdb46088d8857a65d
CyberGrizz88 <cybergrizz88@gmail.com>
2023-08-06 23:14:41 -0400

    Adding HitCounter to cdk

---
+++ CDK-WORKSHOP-JAVA2/src/main/java/com/myorg/HitCounter.java
@@ -0,0 +1,9 @@
+package com.myorg;
+
+import software.constructs.Construct;
+
+public class HitCounter extends Construct {
+    public HitCounter(final Contruct scope, final String id, final HitCounterProps props) {
+        super(scope, id);
+    }
+}
